811ccc266b4f61e86da116d52f07279e3d33e462
Banks Humphreys <iobg0320@gmail.com>
2016-09-19 13:43:46 -0500

    simple regiter and login pages

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,31 @@ const Toppings = require('../models/toppings')
 router.get('/', (req, res) =>
   res.render('index')
 )
+router.get('/login', (req, res) =>
+  res.render('login')
+)
+router.post('/login',(req,res)=>{
+  if(req.body.password==='password'){
+    res.redrect('/')
+  }
+  else{
+    res.render('login', {error:'Email and password do not match'})
+  }
+})
+
+
+router.get('/register',(req,res)=>{
+  res.render('register')
+})
+router.post('/register',(req,res)=>{
+  if(req.body.password===req.body.passwordConfirm){
+    res.redrect('/')
+  }
+  else{
+    res.render('register', {error:'Please confirm your password'})
+  }
+})
+
 
 router.get('/about', (req, res) =>
   res.render('about', { page: 'About' })
@@ -26,6 +51,7 @@ router.post('/contact', (req, res, err) =>
     .then(() => res.redirect('/'))
     .catch(err)
 )
+
 
 router.get('/order', (req, res) =>{
   Size.find({})
